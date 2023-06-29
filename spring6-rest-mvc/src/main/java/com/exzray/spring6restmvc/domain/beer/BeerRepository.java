4dabaf3290cceed8633eb7c6470fcc07b987c0c4
Nazirul Aiman <nazirulaiman6379@gmail.com>
2023-06-29 13:52:04 +0800

    checkpoint 185

--- spring6-rest-mvc/src/main/java/com/exzray/spring6restmvc/domain/beer/BeerRepository.java
+++ spring6-rest-mvc/src/main/java/com/exzray/spring6restmvc/domain/beer/BeerRepository.java
@@ -3,9 +3,11 @@ package com.exzray.spring6restmvc.domain.beer;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.UUID;
 
+@RepositoryRestResource(path = "beer")
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
     Page<Beer> findAllByBeerNameContainsIgnoreCase(String beerName, Pageable pageable);
 }
